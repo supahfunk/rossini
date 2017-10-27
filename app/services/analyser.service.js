d507c39387b7f8b3e2d6132af2c9c5e3c632fb2f
actarian <lzampetti@gmail.com>
2017-10-27 16:52:21 +0200

    rossini

--- app/services/analyser.service.js
+++ app/services/analyser.service.js
@@ -34,6 +34,7 @@
                 service.data = new Uint8Array(bufferLength);
                 // console.log('AnalyserService.attachAnalyser');
             }
+            console.log('pippo');
             return service.data;
         }
 
